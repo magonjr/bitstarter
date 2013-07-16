4e48a0d36379542896e9a4cf4cf1a39717e522c8
magon <magonjr@gmail.com>
2013-07-16 15:01:56 -0300

    hw4 done

--- web.js
+++ web.js
@@ -2,16 +2,16 @@ var express = require('express');
 var app = express.createServer(express.logger());
 var fs= require("fs");
 
-var file = fs.readFileSync("index.html");
-var buffer = new Buffer(file.length);
 
-buffer.write(file.toString()); 
 
 app.get('/', function(request, response) {
+	var file = fs.readFileSync("index.html");
+	var buffer = new Buffer(file.length);
+	buffer.write(file.toString()); 
 	response.send(buffer.toString());
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
 	console.log("Listening on " + port);
 });
