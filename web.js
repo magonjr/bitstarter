9a381c2dd820b26e973001292559122ddc7bab1c
magon <magonjr@gmail.com>
2013-07-15 22:40:05 -0300

    =mudancas

--- web.js
+++ web.js
@@ -1,12 +1,17 @@
 var express = require('express');
-
 var app = express.createServer(express.logger());
+var fs= require("fs");
+
+var file = fs.readFileSync("index.html");
+var buffer = new Buffer(file.length);
+
+buffer.write(file.toString()); 
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+	response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+	console.log("Listening on " + port);
 });
